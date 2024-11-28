05026f263faf3dd73f5e5b6caedec92a2e59c120
Aswin Gopakumar <aswingopakumar04@gmail.com>
2024-11-29 00:38:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import './App.css';
+import run from './gemini';
+
+function App() {
+  const [inputValue, setInputValue] = useState(''); // State for input value
+  const [output, setOutput] = useState(''); // State for API response
+
+  // Function to handle input change
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  // Function to handle API call
+  const handleRun = async () => {
+    if (inputValue.trim() === '') {
+      setOutput('Please enter a prompt.');
+      return;
+    }
+    try {
+      const response = await run(inputValue); // Call the Gemini API
+      setInputValue(''); 
+      setOutput(response); // Update output state with the response
+       
+    } catch (error) {
+      setOutput('An error occurred while fetching the response.');
+      console.error(error);
+    }
+    
+  };
+
+  return (
+    <>
+      <div className="components">
+        <div className="outputField">{output}</div>
+        <div className="searchBox">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Enter your prompt"
+            
+          />
+          <button type='submit' onClick={handleRun}>Send</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
